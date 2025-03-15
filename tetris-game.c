8dfc6878e8f266f96986d8502072a8435d88f553
Janer <jenda.jakub.vlcek@gmail.com>
2025-03-15 19:20:56 +0100

    Implement block structure and enhance game logic for Tetris

--- tetris-game.c
+++ tetris-game.c
@@ -3,6 +3,49 @@
 #include <conio.h>
 #include <stdlib.h>
 
+typedef struct {
+    int shape[4][4];
+    int size;
+} Block;
+
+Block blocks[7] = {{{
+    {0, 0, 0, 0},
+    {1, 1, 1, 1},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+}, 4}, {{
+    {0, 1, 0, 0},
+    {1, 1, 1, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+}, 3}, {{
+    {1, 0, 0, 0},
+    {1, 0, 0, 0},
+    {1, 1, 0, 0},
+    {0, 0, 0, 0}
+}, 3}, {{
+    {0, 0, 1, 0},
+    {0, 0, 1, 0},
+    {0, 1, 1, 0},
+    {0, 0, 0, 0}
+}, 3}, {{
+    {1, 1, 0, 0},
+    {1, 1, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+}, 2}, {{
+    {0, 1, 1, 0},
+    {1, 1, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+}, 3}, {{
+    {1, 1, 0, 0},
+    {0, 1, 1, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+}, 3}};
+
+
 void drawPlayingBoard(int board[20][10]) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD coord;
@@ -24,49 +67,90 @@ void drawPlayingBoard(int board[20][10]) {
     }
 }
 
-void processInput(int* posX, int* posY) {
+void processInput(int* posX, int* posY, Block block) {
+    int ok = 1;
     if (kbhit()) {
         char input = getch();
 
         switch (input) {
-            // case 72: // Up arrow key
-            //     // rotate
-            //     break;
-            case 80: // Down arrow key
-                if (*posY < 19) {
-                    (*posY)++;
-                }
-                break;
             case 75: // Left arrow key
-                if (*posX > 0) {
+                for (int y = 0; y < block.size; y++) {
+                    for (int x = 0; x < block.size; x++) {
+                        if (block.shape[y][x] == 1) {
+                        int boardX = *posX + x;
+                        if (boardX - 1 < 0) {
+                            ok = 0;
+                        }
+                        }
+                    }
+                }
+                if (ok) {
                     (*posX)--;
                 }
                 break;
             case 77: // Right arrow key
-                if (*posX < 9) {
+                for (int y = 0; y < block.size; y++) {
+                    for (int x = 0; x < block.size; x++) {
+                        if (block.shape[y][x] == 1) {
+                        int boardX = *posX + x;
+                        if (boardX + 1 > 9) {
+                            ok = 0;
+                        }
+                        }
+                    }
+                }
+                if (ok) {
                     (*posX)++;
-            }
+                }
                 break;
             case 'q':
                 exit(0);
                 break;
+            // case 72: // Up arrow key
+            //     // rotate
+            //     break;
+            // case 80: // Down arrow key
+            //     if (*posY < 19) {
+            //         (*posY)++;
+            //     }
+            //     break;
             default:
                 break;
         }
     }
 }
 
+void generateNewBlock(int board[20][10], int *posX, int *posY, Block block) {
+    for (int y = 0; y < block.size; y++) {
+        for (int x = 0; x < block.size; x++) {
+            if (block.shape[y][x] == 1) {
+                int boardY = *posY + y;
+                int boardX = *posX + x;
 
-void generateNewBlock(int board[20][10], int *posX, int *posY) {
-    // 1x1 block
-    board[*posY][*posX] = 1;
+                if (boardY < 20 && boardX < 10 && boardY >= 0 && boardX >= 0) {
+                    board[boardY][boardX] = 1;
+                }
+            }
+        }
+    }
 }
 
-int checkCollision(int board[20][10], int *posX, int *posY) {
-    if (*posY < 19 && board[*posY + 1][*posX] == 0) {
-        return 0; 
+int checkCollision(int board[20][10], int *posX, int *posY, Block *block) {
+    int ok = 1;
+    for (int y = 0; y < block -> size; y++) {
+        for (int x = 0; x < block -> size; x++) {
+            if (block -> shape[y][x] == 1) {
+                int boardY = *posY + y;
+                int boardX = *posX + x;
+
+                if (boardY >= 19 || board[boardY + 1][boardX] != 0) {
+                    ok = 0;
+                }
+            }
+        }
     }
-    return 1; 
+    if (ok) return 0; 
+    else return 1; 
 }
 
 void checkLine(int fixed[20][10], int board[20][10]) {
@@ -97,16 +181,28 @@ void checkLine(int fixed[20][10], int board[20][10]) {
     }
 }
 
-void gameLoop(int board[20][10], int *posX, int *posY, int fixed[20][10]) {
-    if (*posY >= 19 || checkCollision(board, posX, posY)) {
-        fixed[*posY][*posX] = 1;
-        board[*posY][*posX] = 1;
+void gameLoop(int board[20][10], int *posX, int *posY, int fixed[20][10], Block *block) {
+    if (*posY >= 19 || checkCollision(board, posX, posY, block)) {
+        for (int y = 0; y < block -> size; y++) {
+            for (int x = 0; x < block -> size; x++) {
+                if (block -> shape[y][x] == 1) {
+                    int boardY = *posY + y;
+                    int boardX = *posX + x;
+    
+                    if (boardY < 20 && boardX < 10 && boardY >= 0 && boardX >= 0) {
+                        fixed[boardY][boardX] = 1;
+                        board[boardY][boardX] = 1;
+                    }
+                }
+            }
+        }
+        *block = blocks[rand() % 7];
         *posX = 4;
         *posY = 0;
     }
     else (*posY)++;
     checkLine(fixed, board);
-    generateNewBlock(board, posX, posY);
+    generateNewBlock(board, posX, posY, *block);
     drawPlayingBoard(board);
 
     for (int y = 0; y < 20; y++) {
@@ -124,9 +220,10 @@ int main() {
     int board[20][10] = {0};
     int fixed[20][10] = {0};
     int posX = 4, posY = 0;
+    Block block = blocks[rand() % 7];
+
     while (1) {
-        processInput(&posX, &posY); 
-        gameLoop(board, &posX, &posY, fixed); 
+        processInput(&posX, &posY, block); 
+        gameLoop(board, &posX, &posY, fixed, &block); 
     }
-    return 0;
 }
